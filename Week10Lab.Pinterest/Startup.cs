a8aca52a9ec97ddd25fb92d9cfad50203f850fcb
BaxleyAldworth <jbaxle@gmail.com>
2015-12-30 14:50:25 -0600

    initial commit

---
+++ Week10Lab.Pinterest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week10Lab.Pinterest.Startup))]
+namespace Week10Lab.Pinterest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
